26c4817ee2d924dc83c45c232fb5516c8c769175
Acer <madubhashinimanusha@gmail.com>
2025-06-28 16:25:02 +0530

    initiate the app.ts and define the HTTP Get request

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
 import express, {Express, Request , Response} from 'express';
-// 1. Initialize the express app
-const app: Express = express();
+import app from "./app";
+// // 1. Initialize the express app
+// const app: Express = express();
 // 2. Define the application port
 const port = 3000;
-// 3. Define a simple HTTP Get Request
-app.get('/',(req : Request, res : Response) => {
-
-    res.send("Hello!")
-
-});
+// // 3. Define a simple HTTP Get Request
+// app.get('/',(req : Request, res : Response) => {
+//
+//     res.send("Hello World!!")
+//
+// });
 // 4. Instructs the express app to listen on port 3000
 app.listen(port,() => {
     console.log(`server is running at http://localhost:${port}`)

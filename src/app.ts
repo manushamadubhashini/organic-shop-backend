a56bea19df75a31c15d444bcf02a3e42ef67739d
Acer <madubhashinimanusha@gmail.com>
2025-06-28 16:35:07 +0530

    Define Middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,12 @@
 import express, {Express, Request, Response} from "express";
-
+// 1. Initialize the express app
 const app: Express = express();
+
+// 2. Define Middlewares
+// 2.1 Instruct to parse the request
+// payload data to be converted
+// to json format
+app.use(express.json());
 
 app.get('/',(req : Request, res : Response) => {
 

fac9e91d8cd6586804e85920e12f5873129189f9
Acer <madubhashinimanusha@gmail.com>
2025-07-05 15:12:13 +0530

    product relatit CRUD operation define

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,7 +1,8 @@
 // controller function to handel getAll product
 import express from "express";
 import {Request,Response} from "express";
-import * as productService from '../services/product.service' // product service eken ena full result eka productService namin include karagnn
+import * as productService from '../services/product.service'
+import productRouter from "../routes/product.routes"; // product service eken ena full result eka productService namin include karagnn
 
 export const getAllProducts = (req : Request, res : Response) => {
 
@@ -41,12 +42,62 @@ export const saveProduct = (req : Request,res : Response) =>{
 }
 
 export const  getProduct = (req : Request , res :Response) =>{
+    const productId = parseInt(req.params.id);
+    if (isNaN(productId)){
+        res.status(400).json({
+            error : "Invalid Product Id"
+        });
+        return;
+    }
+
+    const product = productService.getProductById(productId)
+    if(!product){
+        res.status(404).json({
+            error : "Product Not Found"
+        });
+        return;
+    }
+    res.status(200).json(product)
 
 }
 
 export const updateProduct = (req : Request, res : Response) => {
+    const productId = parseInt(req.params.id);
+    if (isNaN(productId)){
+        res.status(400).json({
+            error : "Invalid Product Id"
+        });
+        return;
+    }
+    const updatedData = req.body;
+    const updatedProduct = productService.updateProduct(productId,updatedData);
+    if (!updatedProduct){
+        res.status(404).json({
+            error : "Product Not Found"
+        });
+        return;
+    }
+    res.status(200).json(updatedProduct);
 
 }
 export const deleteProduct = (req : Request, res : Response) => {
+    const productId = parseInt(req.params.id);
+    if (isNaN(productId)){
+        res.status(400).json({
+            error : "Invalid Product Id"
+        });
+        return;
+    }
+    const deleteProducts = productService.deleteProduct(productId)
+
+    if(!deleteProducts){
+        res.status(404).json({
+            error : "Product Not Found"
+        })
+        return;
+    }
+    res.status(200).json({
+        message : "Product Deleted Successfully"
+    })
 
 }
